b808037dd8ff069ff1692da679e9e77168ca0c24
Denis <denisfolyush@gmail.com>
2026-06-29 20:23:45 +0300

    SKEL-31-change jira api version to 2 insted of 3, add agent and agent's response go to discord webhook

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import requests          
 import json              
+import time 
 from requests.auth import HTTPBasicAuth 
 from google import genai 
 import os
@@ -14,7 +15,7 @@ GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')
 DISCORD_WEBHOOK_URL = os.getenv('DISCORD_WEBHOOK_URL')
 
 client = genai.Client(api_key=GEMINI_API_KEY)
-url = "https://vitaliyoliyniyk.atlassian.net/rest/api/3/search/jql"
+url = "https://vitaliyoliyniyk.atlassian.net/rest/api/2/search/jql"
 
 auth = HTTPBasicAuth(JIRA_EMAIL, JIRA_API_TOKEN)
 
@@ -26,7 +27,7 @@ query = {
 #   'nextPageToken': '<string>',
 #   'maxResults': '10',
   'fields': 'summary,assignee,status,description,CATEGORY,text,comment',
-#   'expand': '<string>',
+  'expand': 'renderedFields'
   # 'reconcileIssues': '{reconcileIssues}'
 }
 
@@ -42,47 +43,61 @@ response_data =  response.json()
 
 
 with open('data.json', 'w') as f:
-    json.dump(response_data, f, indent=4,)
+    json.dump(response_data, f, indent=2,)
 
 response_data =  response.json()
 
 def json_text_extractor(json_data):
-
+  all_tickets_text = ""
   for ticket in json_data.get('issues', []):
       ticket_key = ticket.get('key')      
       fields = ticket.get('fields', {})
+
+      assignee = fields.get('assignee') or {}
+      user =  assignee.get('displayName', 'Unassigned') 
       
       summary = fields.get('summary')    
-      raw_description = fields.get('description') or {}
-      clean_description = ""
-      
-      for block in raw_description.get('content', []):
-          for item in block.get('content', []):
-              if item.get('type') == 'text':
-                  clean_description += item.get('text', '') + " "
+      description = fields.get('description') or {}
                   
-      print(f"Tiket name is {ticket_key}, \n short task summary :{summary}, \n Discription:{clean_description}")
-      print("-----------------------------------------")
+      ticket_block = (
+            f"Tiket name is {ticket_key} owner {user}, \n"
+            f"Short task summary :{summary}, \n"
+            f"Discription:{description}\n"
+            f"------------------------------------------------------\n"
+
+        )
+      
+      print(ticket_block, end="")
+      all_tickets_text += ticket_block
+  return all_tickets_text
+
+json_out =json_text_extractor(response_data)
+def agent(jira_tickets):
+        gem_response = client.models.generate_content(
+        model='gemini-2.5-flash',
+        contents={'text': f' {json_out}can pls give like a simple grade for each ticket and tell ehy its good ot bad CRITICAL Must be 2000 or fewer in length'},
+        # config={
+        #     'temperature': 0,
+        #     'top_p': 0.95,
+        #     'top_k': 20,
+        # },
+    )
+        return gem_response
+
+gemini_response=agent(json_out)
+print(gemini_response)
+
+cut_report = gemini_response.text[:1890]
+
+data = {
+    "content": f"here is daily report {cut_report}"
+}
 
 
-json_out = json_text_extractor(response_data)
-
-# upload_file = genai.client.file.upload(file=data.txt)
-
-# with open('data.txt', 'w') as f:
-#     f.write(json_out)
+discord_response = requests.post(DISCORD_WEBHOOK_URL, json=data)
 
 
-# uploaded_file = client.files.upload(file='data.txt')
-
-# gem_response = client.models.generate_content(
-#     model='gemini-2.5-flash',
-#     contents={'text': f' {json_out}can pls give like a simple grade for each ticket and tell ehy its good ot bad not extra text'},
-#     # config={
-#     #     'temperature': 0,
-#     #     'top_p': 0.95,
-#     #     'top_k': 20,
-#     # },
-# )
-
-# print(gem_response)
+if discord_response.status_code == 204:
+    print("message secesfully send ")
+else:
+    print(f"error code ={discord_response.status_code}{discord_response.text}")
